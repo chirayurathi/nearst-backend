a0c60167d59988f6c20eacfdfb2e85596c53da46
chirayurathi <chirayurathi4@gmail.com>
2020-12-15 03:08:52 +0530

    id

--- invoice/views.py
+++ invoice/views.py
@@ -4,13 +4,38 @@ from .utils import render_to_pdf
 from datetime import date
 from django.views.decorators.csrf import csrf_exempt
 import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate('serviceKey.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 @csrf_exempt
 def GeneratePdf(request):
-    print('this',request.body.decode('utf-8'))
     data = json.loads(request.body.decode('utf-8'))
-    data['today'] = date.today()
-    pdf = render_to_pdf('pdf/invoice.html', data)
+    order_ref = db.collection(u'orders').document(data["id"])
+    docs = order_ref.get()
+    docs = docs.to_dict()
+    user_ref = db.collection(u'User').document(docs["userId"])
+    docsU = user_ref.get()
+    docsU = docsU.to_dict()
+    seller_ref = db.collection(u'merchant').document(docs["merchantId"])
+    docsM = seller_ref.get()
+    docsM = docsM.to_dict()
+    sendData = {
+        'order_id':data["id"],
+        'customer_name:':docsU["userName"],
+        'customer_ph':docsU["userPhone"],
+        'customer_add':docs["address"],
+        'seller_name':docsM["storeName"],
+        'seller_gst':"",
+        'seller_ph':docsM["landlineNumber"],
+        'seller_add':docsM["address"],
+        'products':docs["products"],
+        'product_final':docs['totalAmount'],
+        'today':docs["createdAt"]
+    }
+    print(str(sendData))
+    pdf = render_to_pdf('pdf/invoice.html', sendData)
     response = HttpResponse(pdf, content_type='application/pdf')
-    # response['Access-Control-Allow-Origin'] = '*'
-    # response['Access-Control-Allow-Headers'] = 'content-type, Access-Control-Allow-Origin'
     return response
